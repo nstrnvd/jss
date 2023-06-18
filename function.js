b2fe223194aef958901576479615868ed2bdb50b
Nastaran Valizadeh <nastaranvalizade2@gmail.com>
2023-06-18 11:40:00 +0330

    first commit

---
+++ function.js
@@ -0,0 +1,35 @@
+// function min(a, b) {
+//   if (a < b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+// function min(a, b) {
+//   return a < b ? a : b;
+// }
+
+
+
+// let age = 12;
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Did parents allow you?');
+//   }
+// }
+// checkAge(age);
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   }
+//   return confirm('Did parents allow you?');
+// }
+// checkAge(age);
+
+
+// function checkAge(age) {
+//   return (age > 18) ? true : confirm('Did parents allow you?');
+// }
---
+++ loops.js
@@ -0,0 +1,11 @@
+// let i = 3;
+// while (i) {
+//   alert( i-- );
+// }
+
+
+// let i = 0;
+// while (i < 3) {
+//   alert("number " + i + "!");
+//   i++;
+// }
